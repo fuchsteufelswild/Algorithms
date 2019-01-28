f39719c3766764e91071c785c8eb9dcbef34676e
Alper Canal <47023800+fuchsteufelswild@users.noreply.github.com>
2019-01-28 15:28:56 +0300

    Create Odd-Even Subarrays.cpp

---
+++ HackerEarth/Dynamic Programming/Introduction to Dynamic Programming 1/Odd-Even Subarrays.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+int main()
+{
+    std::map<int, long long> memo;
+    int n;
+    std::cin >> n;
+    long long res = 0;
+    
+    std::vector<int> vec(n);
+    for(int i = 0; i < n; ++i)
+    {
+        int temp;
+        std::cin >> temp;
+        if(temp % 2)  // If even assign 1
+            vec[i] = 1;
+        else          // If odd assign -1
+            vec[i] = -1;
+    }
+    memo.insert(std::pair<int, long long>(0, 1));
+    
+    for(int i = 0; i < n; ++i) // Find number of subarrays s.t sum = 0
+    {
+        if(i != 0)
+            vec[i] += vec[i - 1]; // Sum till ith index
+        if(memo.find(vec[i]) == memo.end()) // If that sum is not seen before
+            memo.insert(std::pair<int, long long>(vec[i], 1));
+        else
+        {
+            res += memo[vec[i]];
+            ++memo[vec[i]];
+            
+        }
+    }
+    
+    std::cout << res << std::endl;
+    
+    return 0;
+}
